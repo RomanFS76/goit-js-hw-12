84b3db6aeb5e61bc7650782486225af6cb222071
RomanFS76 <korvy85@gmail.com>
2024-04-17 15:14:31 +0300

    delete then&catch - main.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,62 +1,18 @@
-import { renderImages } from './render-functions.js';
-
 import axios from 'axios';
-
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const URLservice = 'https://pixabay.com/api/';
 const API_KEY = '43330031-9673f4a92262d12e3841226eb';
 
-const loaderEl = document.querySelector('span');
-const gallaryEl = document.querySelector('.gallary');
-
-const lightbox = new SimpleLightbox('.gallary a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
-
-let page = 1;
-
-export async function getPhotos(inputValue, page = 1) {
-  const { data } = await axios.get(URLservice, {
-    params: {
-      key: API_KEY,
-      q: inputValue,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: 15,
-      page,
-    },
-  });
-
-  loaderEl.classList.add('visually-hidden');
-
-  if (inputValue === '') {
-    iziToast.show({
-      message: 'Field must be filled!',
-      color: 'green', // blue, red, green, yellow
-      position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
-      timeout: 2000,
+export async function getPhotos(inputValue) {
+    const { data } = await axios(URLservice, {
+      params: {
+        key: API_KEY,
+        q: inputValue,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 15,
+      },
     });
-    return;
-  }
-  if (Object.keys(data.hits).length === 0) {
-    iziToast.show({
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      color: 'blue', // blue, red, green, yellow
-      position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
-      timeout: 3000,
-    });
-  }
-  gallaryEl.insertAdjacentHTML('beforeend', renderImages(data.hits));
-  lightbox.refresh();
-  formEl.reset();
-
-  return data;
+    return data;
 }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,52 @@
-import { getPhotos } from './js/pixabay-api.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { getPhotos } from './js/pixabay-api.js';
+import { renderImages } from './js/render-functions.js';
 
 const formEl = document.querySelector('.form-search');
 const gallaryEl = document.querySelector('.gallary');
-
 const loaderEl = document.querySelector('span');
+const lightbox = new SimpleLightbox('.gallary a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-
-formEl.addEventListener('submit', event => {
+formEl.addEventListener('submit', async event => {
   event.preventDefault();
   const inputValue = event.target.elements.input.value;
 
   loaderEl.classList.remove('visually-hidden');
   gallaryEl.innerHTML = '';
 
-  getPhotos(inputValue)
-  
-})
+  try {
+    const response = await getPhotos(inputValue);
+    loaderEl.classList.add('visually-hidden');
+    if (inputValue === '') {
+      iziToast.show({
+        message: 'Field must be filled!',
+        color: 'green', // blue, red, green, yellow
+        position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
+        timeout: 2000,
+      });
+      return;
+    }
+    if (Object.keys(response.hits).length === 0) {
+      iziToast.show({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        color: 'blue', // blue, red, green, yellow
+        position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
+        timeout: 3000,
+      });
+    }
+    gallaryEl.insertAdjacentHTML('beforeend', renderImages(response.hits));
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error.message);
+  }
+  formEl.reset();
+});
