9ad835e47b08d0b1d1f826c604750676fe3a7749
RR <korvy85@gmail.com>
2024-04-18 22:35:12 +0300

    33

--- src/main.js
+++ src/main.js
@@ -26,8 +26,10 @@ formEl.addEventListener('submit', async event => {
 
   try {
     const response = await getPhotos(inputValue);
+    console.log(response);
     loaderEl.classList.add('visually-hidden');
     if (inputValue === '') {
+      btnLoaderMoreEl.classList.add('visually-hidden');
       iziToast.show({
         message: 'Field must be filled!',
         color: 'green', // blue, red, green, yellow
@@ -37,6 +39,7 @@ formEl.addEventListener('submit', async event => {
       return;
     }
     if (Object.keys(response.hits).length === 0) {
+      btnLoaderMoreEl.classList.add('visually-hidden');
       iziToast.show({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
@@ -44,34 +47,49 @@ formEl.addEventListener('submit', async event => {
         position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
         timeout: 3000,
       });
+      formEl.reset();
+      return;
     }
 
     gallaryEl.insertAdjacentHTML('beforeend', renderImages(response.hits));
     lightbox.refresh();
-
     btnLoaderMoreEl.classList.remove('visually-hidden');
   } catch (error) {
     console.log(error.message);
   }
-  formEl.reset();
+  // formEl.reset();
 });
 
-let page = 1;
+let page = 2;
 
 btnLoaderMoreEl.addEventListener('click', async () => {
-  // loaderEl.classList.remove('visually-hidden');
-  
- console.log(page);
+  loaderEl.classList.remove('visually-hidden');
+  btnLoaderMoreEl.disabled = true;
+
   const nextPage = await getPhotos(page);
+
   page += 1;
-  console.log(page);
 
   gallaryEl.insertAdjacentHTML('beforeend', renderImages(nextPage.hits));
 
+  loaderEl.classList.add('visually-hidden');
+  btnLoaderMoreEl.disabled = false;
+
   const totalPage = Math.ceil(nextPage.totalHits / nextPage.hits.length);
 
-
   if (page >= totalPage) {
-    loaderEl.classList.add('visually-hidden');
+    btnLoaderMoreEl.classList.add('visually-hidden');
   }
 });
+
+// getBoundingClientRect();
+
+// let elem = document.querySelector("div");
+// let rect = elem.getBoundingClientRect();
+// for (const key in rect) {
+//   if (typeof rect[key] !== "function") {
+//     let para = document.createElement("p");
+//     para.textContent = `${key} : ${rect[key]}`;
+//     document.body.appendChild(para);
+//   }
+// }
